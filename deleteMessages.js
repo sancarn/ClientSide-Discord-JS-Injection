547b09af45776e89bff51cb7641d2cdbe34df500
Sancarn <sancarn@hotmail.co.uk>
2017-09-15 22:34:44 +0100

    Update deleteMessages.js

--- deleteMessages.js
+++ deleteMessages.js
@@ -5,7 +5,7 @@ function deleteMessages(count){
         if(message.getElementsByClassName("btn-option").length){
             message.getElementsByClassName("btn-option")[0].click()
             $('.option-popout.small-popout-box').lastChild.click()
-            $('.contents-4L4hQM')[0].click()
+            $('.contents-4L4hQM').click()
             document.documentElement.click()
         }
     }
