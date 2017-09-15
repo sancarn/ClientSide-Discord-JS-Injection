452c1720131b949d50853bf85f7f9ca14c2513d9
Sancarn <sancarn@hotmail.co.uk>
2017-09-15 23:27:55 +0100

    Update deleteMessages.js

--- deleteMessages.js
+++ deleteMessages.js
@@ -2,10 +2,12 @@
 function deleteMessages(count){
     var i=0;
     var interval = setInterval(function($){
-            if(i++==count){
+            if(i==count){
                 clearInterval(interval);
             } else {
+                var msgID = getLatestMsgID() 
                 deleteLatest($);
+                if(msgID!=getLatestMsgID()) i++;
             }
     }.bind(window,$),500)
 }
@@ -18,4 +20,9 @@ function deleteLatest($){
         $('.contents-4L4hQM').click()
         document.documentElement.click()
     }
+}
+
+function getLatestMsgID(){
+    var msg = Array.from(document.getElementsByClassName('message')).pop()
+    return msg.__reactInternalInstance$vujmm9ix6eoaw6u98gkd6xbt9._domID
 }
