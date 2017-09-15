9538050ab6b6ba80c62c0a2273a2f79096926ccc
Sancarn <sancarn@hotmail.co.uk>
2017-09-15 23:11:09 +0100

    Update deleteMessages.js

--- deleteMessages.js
+++ deleteMessages.js
@@ -1,18 +1,21 @@
 function deleteMessages(count){
     var i=0;
-    var interval = setInterval(function(){
-        if(i++==count){
-            clearInterval(interval);
-        } else {
-            deleteLatest();
-        }
-    },100)
+    var interval = setInterval(function($){
+        try {
+            if(i==count){
+                clearInterval(interval);
+            } else {
+                deleteLatest($);
+            }
+            i++;
+        } catch (e) {
+        }   
+    }.bind(window,$),500)
 }
 
 
-function deleteLatest(){
-    var messages = document.getElementsByClassName('message');
-    message = messages[messages.length-1 - i]
+function deleteLatest($){
+    var message = Array.from(document.getElementsByClassName('message')).pop()
     if(message.getElementsByClassName("btn-option").length){
         message.getElementsByClassName("btn-option")[0].click()
         $('.option-popout.small-popout-box').lastChild.click()
