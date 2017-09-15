6e6e6bb8936d0c5178fdc1180065bf44223b87d1
Sancarn <sancarn@hotmail.co.uk>
2017-09-15 22:54:48 +0100

    Update deleteMessages.js

--- deleteMessages.js
+++ deleteMessages.js
@@ -1,12 +1,22 @@
 function deleteMessages(count){
-    var messages = document.getElementsByClassName('message');
-    for(var i=0; i<count;i++){
-        message = messages[messages.length-1 - i]
-        if(message.getElementsByClassName("btn-option").length){
-            message.getElementsByClassName("btn-option")[0].click()
-            $('.option-popout.small-popout-box').lastChild.click()
-            $('.contents-4L4hQM').click()
-            document.documentElement.click()
+    var i=0;
+    var interval = setInterval(function(){
+        if(i++==count){
+            clearInterval(interval);
+        } else {
+            deleteLatest();
         }
+    },100)
+}
+
+
+function deleteLatest(){
+    var messages = document.getElementsByClassName('message');
+    message = messages[messages.length-1 - i]
+    if(message.getElementsByClassName("btn-option").length){
+        message.getElementsByClassName("btn-option")[0].click()
+        $('.option-popout.small-popout-box').lastChild.click()
+        $('.contents-4L4hQM').click()
+        document.documentElement.click()
     }
 }
