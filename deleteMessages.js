844b677089814dd2d312935740aa63ff176b10df
Sancarn <sancarn@hotmail.co.uk>
2017-09-16 02:42:14 +0100

    Update deleteMessages.js

--- deleteMessages.js
+++ deleteMessages.js
@@ -27,4 +27,22 @@ better solution would be to use:
 HTTPGet("https://discordapp.com/api/v6/channels/{channel-id}/messages?limit=50")
 and then from there use
 HTTPDelete("https://discordapp.com/api/v6/channels/{channel-id}/messages/{message-id}")
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+MORE INFO ON THIS TOPIC:
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function reqListener () {
+  console.log(this.responseText);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET","https://discordapp.com/api/v6/channels/" + /channels\/(\d+)\/(\d+)/.exec(window.location.pathname)[2] + "/messages?limit=50")
+oReq.setRequestHeader("authorization",oAuthToken)
+oReq.send()
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Note oAuthToken is required to use this method. The authorization token can be found in Developer Tools > Application Tab > Token key. You can double click the icon and copy the path
+
 */
